92625970aa605e5392fa933151287a69082cdbb6
haru-labo <harukun.space@gmail.com>
2019-06-24 22:57:14 +0900

    ボタン追加

--- resources/views/layouts/mypage.blade.php
+++ resources/views/layouts/mypage.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- pure css --}}
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <title>@yield('title')</title>
 </head>
 <body>
--- resources/views/mypages/index.blade.php
+++ resources/views/mypages/index.blade.php
@@ -3,14 +3,18 @@
 @section('title', 'DailyUseItems')
 
 @section('content')
-<!-- createNewItemForm -->
+<!-- modal start createNewItemForm -->
     <from action="/additem" method="POST">
         {{ csrf_field() }}
 
-        <div>
-            <label for="">追加</label>
-        </div>
     </from>
+<!-- modal end createNewItemForm -->
+    <a class="button-secondary pure-button" href="#"><i class="fas fa-plus-circle"></i>追加</a>
+    <form class="pure-form" action="/catserch">
+        {{ csrf_field() }}
+        <input type="text" class="pure-input-rounded">
+        <button type="submit" class="pure-button">CategorySearch</button>
+    </form>
 <!-- itemList -->
     <table class="pure-table">
         <tr>
@@ -18,6 +22,8 @@
             <th>品名</th>
             <th>ストック</th>
             <th>開封日</th>
+            <th>消費量（/日）</th>
+            <th>編集／削除</th>
         </tr>
         @foreach ($items as $item)
         @if ($loop->iteration % 2 == 0)
@@ -29,6 +35,13 @@
             <td>{{$item->name}}</td>
             <td>{{$item->stock}}</td>
             <td>{{$item->dateopen}}</td>
+            <td></td>
+            <td>
+                <button class="button-warning pure-button">編集</button>
+                <button class="button-error pure-button">
+                    <i class="fa fa-trash"></i>削除
+                </button>
+            </td>
         </tr>
         @endforeach
     </table>
